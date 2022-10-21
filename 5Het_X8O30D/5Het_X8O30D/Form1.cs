31bd31bee17a5ce761e9aa0361357522ae588026
Zsuzsanna Nagy <zsuzsanna.nagy3@stud.uni-corvinus.hu>
2022-10-21 12:33:39 +0200

    6.3feladat

--- 5Het_X8O30D/5Het_X8O30D/Form1.cs
+++ 5Het_X8O30D/5Het_X8O30D/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using _5Het_X8O30D.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,25 @@ namespace _5Het_X8O30D
         public Form1()
         {
             InitializeComponent();
+
+            ExchangeRate();
+
+
+        }
+
+        void ExchangeRate()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
