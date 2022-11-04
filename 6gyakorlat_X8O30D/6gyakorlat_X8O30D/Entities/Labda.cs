147f6d15360ed6c2a04ff533763593060519298d
Zsuzsanna Nagy <zsuzsanna.nagy3@stud.uni-corvinus.hu>
2022-11-04 14:05:27 +0100

    6gy.2

---
+++ 6gyakorlat_X8O30D/6gyakorlat_X8O30D/Entities/Labda.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6gyakorlat_X8O30D.Entities
+{
+    internal class Labda : Label
+    {
+        public Labda()
+        {
+            AutoSize = false;
+            Height = 50;
+            Width = 50;
+
+            Paint += Labda_Paint;
+        }
+
+        private void Labda_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
