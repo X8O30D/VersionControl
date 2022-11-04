432e5cd46f59649b0afd9bac08124099afa61e0f
Zsuzsanna Nagy <zsuzsanna.nagy3@stud.uni-corvinus.hu>
2022-11-04 15:58:21 +0100

    6gy.12

--- 6gyakorlat_X8O30D/6gyakorlat_X8O30D/Form1.Designer.cs
+++ 6gyakorlat_X8O30D/6gyakorlat_X8O30D/Form1.Designer.cs
@@ -30,17 +30,19 @@
         {
             this.components = new System.ComponentModel.Container();
             this.mainpanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ballbutton = new System.Windows.Forms.Button();
+            this.carbutton = new System.Windows.Forms.Button();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
-            this.carbutton = new System.Windows.Forms.Button();
-            this.ballbutton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.mainpanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainpanel
             // 
             this.mainpanel.AllowDrop = true;
+            this.mainpanel.Controls.Add(this.button1);
             this.mainpanel.Controls.Add(this.label1);
             this.mainpanel.Controls.Add(this.ballbutton);
             this.mainpanel.Controls.Add(this.carbutton);
@@ -48,6 +50,35 @@
             this.mainpanel.Name = "mainpanel";
             this.mainpanel.Size = new System.Drawing.Size(1120, 699);
             this.mainpanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Coming next:";
+            // 
+            // ballbutton
+            // 
+            this.ballbutton.Location = new System.Drawing.Point(137, 21);
+            this.ballbutton.Name = "ballbutton";
+            this.ballbutton.Size = new System.Drawing.Size(92, 92);
+            this.ballbutton.TabIndex = 1;
+            this.ballbutton.Text = "BALL";
+            this.ballbutton.UseVisualStyleBackColor = true;
+            this.ballbutton.Click += new System.EventHandler(this.ballbutton_Click);
+            // 
+            // carbutton
+            // 
+            this.carbutton.Location = new System.Drawing.Point(27, 21);
+            this.carbutton.Name = "carbutton";
+            this.carbutton.Size = new System.Drawing.Size(92, 92);
+            this.carbutton.TabIndex = 0;
+            this.carbutton.Text = "CAR";
+            this.carbutton.UseVisualStyleBackColor = true;
+            this.carbutton.Click += new System.EventHandler(this.carbutton_Click);
             // 
             // createTimer
             // 
@@ -61,34 +92,15 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
-            // carbutton
+            // button1
             // 
-            this.carbutton.Location = new System.Drawing.Point(27, 21);
-            this.carbutton.Name = "carbutton";
-            this.carbutton.Size = new System.Drawing.Size(92, 92);
-            this.carbutton.TabIndex = 0;
-            this.carbutton.Text = "CAR";
-            this.carbutton.UseVisualStyleBackColor = true;
-            this.carbutton.Click += new System.EventHandler(this.carbutton_Click);
-            // 
-            // ballbutton
-            // 
-            this.ballbutton.Location = new System.Drawing.Point(137, 21);
-            this.ballbutton.Name = "ballbutton";
-            this.ballbutton.Size = new System.Drawing.Size(92, 92);
-            this.ballbutton.TabIndex = 1;
-            this.ballbutton.Text = "BALL";
-            this.ballbutton.UseVisualStyleBackColor = true;
-            this.ballbutton.Click += new System.EventHandler(this.ballbutton_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(298, 67);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 20);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Coming next:";
+            this.button1.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button1.Location = new System.Drawing.Point(97, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 46);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -112,6 +124,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button ballbutton;
         private System.Windows.Forms.Button carbutton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
