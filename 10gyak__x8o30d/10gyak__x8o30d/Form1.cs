330ed106448c123cc24b3b1c5b7a074b84953a7f
Zsuzsanna Nagy <zsuzsanna.nagy3@stud.uni-corvinus.hu>
2022-12-09 14:29:35 +0100

    10.3

--- 10gyak__x8o30d/10gyak__x8o30d/Form1.cs
+++ 10gyak__x8o30d/10gyak__x8o30d/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace _10gyak__x8o30d
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
         public Form1()
         {
             InitializeComponent();
