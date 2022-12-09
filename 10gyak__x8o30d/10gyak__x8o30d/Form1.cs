1c723beeb1eb53ae6a0bc185031dc3db7f20056d
Zsuzsanna Nagy <zsuzsanna.nagy3@stud.uni-corvinus.hu>
2022-12-09 14:35:22 +0100

    10.6

--- 10gyak__x8o30d/10gyak__x8o30d/Form1.cs
+++ 10gyak__x8o30d/10gyak__x8o30d/Form1.cs
@@ -16,7 +16,10 @@ namespace _10gyak__x8o30d
         GameController gc = new GameController();
         GameArea ga;
 
-        
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
 
         public Form1()
         {
@@ -25,8 +28,6 @@ namespace _10gyak__x8o30d
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
-            gc.Start(true);
         }
     }
 }
